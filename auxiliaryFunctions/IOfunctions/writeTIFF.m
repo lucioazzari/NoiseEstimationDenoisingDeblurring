afcf031382dfa0742d7ddc533494ceec3af10194
Lucio Azzari <lucio.azzari@tuni.fi>
2022-06-20 11:57:39 +0300

    debug

--- auxiliaryFunctions/IOfunctions/writeTIFF.m
+++ auxiliaryFunctions/IOfunctions/writeTIFF.m
@@ -1,6 +1,8 @@
 function writeTIFF(data,filePath)
 
+data = uint16(data);
+
 imwrite(data(:,:,1), filePath)
-for ii=1:size(data,3)
+for ii=2:size(data,3)
     imwrite(data(:,:,ii), filePath, 'writemode', 'append')
 end
