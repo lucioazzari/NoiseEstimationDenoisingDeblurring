89adcfe84b52bdd5428490337bd89d86450117c2
Lucio Azzari <lucio.azzari@gmail.com>
2022-08-30 15:39:19 +0300

    change test sequence

--- demo.m
+++ demo.m
@@ -27,14 +27,9 @@ checkDependencies();
 
 %% create synthetic data
 % read Matlab built-in video
-vidObj = VideoReader('xylophone.mp4');
-vidframes = read(vidObj,[100 Inf]);
-sz = size(vidframes);
-% convert RGB frames to grayscale
-data = zeros(sz([1,2,4]));
-for indFrame=1:sz(end)
-    data(:,:,indFrame) = im2double(rgb2gray(vidframes(:,:,:,indFrame)));
-end
+vidObj = VideoReader('data/testSequence.avi');
+vidframes = read(vidObj);
+data = im2double(squeeze(vidframes));
 clear vidframes sz
 
 %% add noise
@@ -54,10 +49,10 @@ outputPath = 'processedSequence.tif';
 [noisy,denoised,deblurred] = processData(demoSequencePath,outputPath,processingType);
 
 %% equivalent processing with custom parameters
-optionalParams.maxBinSize = 1;
-optionalParams.filterStrenght = 1;
-optionalParams.enableEstimationPSD = false;
-optionalParams.deblurringStrenght = 1;
-
-outputPath = 'processedSequenceOpt.tif';
-[noisyOpt,denoisedOpt,deblurredOpt] = processData(demoSequencePath,outputPath,processingType,optionalParams);
+% optionalParams.maxBinSize = 1;
+% optionalParams.filterStrenght = 1;
+% optionalParams.enableEstimationPSD = false;
+% optionalParams.deblurringStrenght = 1;
+% 
+% outputPath = 'processedSequenceOpt.tif';
+% [noisyOpt,denoisedOpt,deblurredOpt] = processData(demoSequencePath,outputPath,processingType,optionalParams);
