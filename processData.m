c22ef9171bbdc63b4bf8d44142d77adf018dd476
Lucio Azzari <lucio.azzari@tuni.fi>
2022-08-31 10:34:56 +0300

    Update processData.m
    Update documentation

--- processData.m
+++ processData.m
@@ -1,57 +1,83 @@
 function [noisy,denoised,deblurred] = processData(inputPath,outputPath,processingType,optionalParams)
-% [noisy,denoised,deblurred] = processData(inputPath,outputPath,processingType,optionalParams)
-% 
-% INPUT:
-%     inputPath: path to the signle-channel gray-scale multipage TIFF to be processed
-%     outputPath: path where to store the signle-channel gray-scale multipage TIFF output
-%     processingType [string]: processing block to be applied to the input ('noiseEst','denoising','all')
-%     optionalParams: optional parameters for each processing block (see below for all the options)
-% 
-% OUTPUT:
-%     noisy: noisy data read from inputPath
-%     denoised: denoised data (if denoising is executed, otherwise empty)
-%     deblurred: deblurred data (if deblurring is executed, otherwise empty)
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %
-% *----------------------------------------------------------------------------------------------------------------------------------*
-% 
-% Processing types:
-% 1) noise estiation + denoising + deblurring: 'all' [default]
-% 2) automatic noise parameters estimation: 'noiseEst'
-% 3) noise estiation + denoising: 'denoising'
-% 
-% *----------------------------------------------------------------------------------------------------------------------------------*
-% 
-% NOISE ESTIMATION PARAMETERS
-% 
-% optionalParams.noiseParamsPath is the path where the user wants the noise parameters to be saved. They will be saved in mat format and can be loaded in Matlab afterwards. The parameters will be saved as affine noise parameters (a and b), and a noise PSD.
-% NOTE: If processingType is set to 'noiseEst' and optionalParams.noiseParamsPath is not specified the noise parameters are saved in the working folder as 'noiseParameters.mat'
-% 
-% *----------------------------------------------------------------------------------------------------------------------------------*
-% 
-% DENOISING PARAMETERS
-% 
-% optionalParams.filterStrenght: adjusts the filter strenght. 
-% optionalParams.filterStrenght is a positive scalar double that will be used to adjust the filter strenght. It is indepepndent from optionalParams.maxBinSize (see below), and it is applied to each denoising scale.
-% 0 corresponds to no denoising, 1 corresponds to standard denoising, >1 corresponds to strong denoising
-% 
-% optionalParams.maxBinSize: is the bin size (positive scalar integer) corresponding to the smallest scale (in multiscale processing) used for denoising. 
-% optionalParams.maxBinSize should be set to 1 most of the time. In case the noise is very strong, and the underlying noise-free signal is barely visible, then we recommend optionalParams.maxBinSize > 1.
-% NOTE: optionalParams.maxBinSize must be odd and smaller or equal than 5; if not, the algorithm automatically converts it to the smallest odd value closest to the input and larger or equal than 1.
-% For 'ordinary' noise strength (e.g., noise variance smaller than half of the data's dynamic range) optionalParams.maxBinSize=1 is recommended. 
-% For 'strong' noise strength (e.g., noise variance between half and the data's dynamic range) optionalParams.maxBinSize=3 is recommended. 
-% For 'very strong' noise strength (e.g., noise variance larger than the data's dynamic range) optionalParams.maxBinSize=5 is recommended.
-% If optionalParams.maxBinSize is not specified, the algorithm will estimate it internally.
-% 
-% *----------------------------------------------------------------------------------------------------------------------------------*
-% 
-% DEBLURRING PARAMETERS
-% 
-% optionalParams.deblurringStrenght: adjusts the deblurring PSF width. Only used if optionalParams.PSF is not specified.
-% optionalParams.deblurringStrenght is a strictly positive scalar double that will be used to adjust the width of the default PSF used for deblurring.
-% 
-% optionalParams.PSF: is the PSF to be used for deblurring. If given as input it overwrites the default isotropic PSF and it disables any optionalParams.deblurringStrenght given as input.
-% 
-% *----------------------------------------------------------------------------------------------------------------------------------*
+%  processData  is a collection of algorithms for noise estimation, denoising, and deblurring
+%
+%
+%  FUNCTION INTERFACE:
+%
+%  [noisy,denoised,deblurred] = processData(inputPath,outputPath,processingType,optionalParams)
+%
+%  INPUT ARGUMENTS:
+%
+%  -- required --
+%
+%    'inputPath' : path to the signle-channel gray-scale multipage TIFF to be processed.
+%
+%   'outputPath' : path where to store the signle-channel gray-scale multipage TIFF output.
+%
+%   'noiseModel' : 'white' --> denoising of white noise (noise PSD disabled)
+%                  'colored' --> denoising of colored noise (noise PSD enabled)
+%
+%
+% -- optional --
+%
+%    'optionalParams.filterStrenght' : adjusts the filter strenght. It must be a positive scalar that 
+%                                      will be used to adjust the filter strenght. It is applied to 
+%                                      each denoising scale.
+%
+%        'optionalParams.maxBinSize' : bin size (positive scalar integer) corresponding to the smallest 
+%                                      scale (in multiscale processing) used for denoising. 
+%                                      optionalParams.maxBinSize should be set to 1 most of the time. 
+%                                      In case the noise is very strong (low SNR), then it is 
+%                                      recommended optionalParams.maxBinSize > 1:
+%                                        - For 'ordinary' noise strength (e.g., noise variance smaller 
+%                                          than half of the data's dynamic range) optionalParams.maxBinSize=1 
+%                                          is recommended. 
+%                                        - For 'strong' noise strength (e.g., noise variance between half 
+%                                          and the data's dynamic range) optionalParams.maxBinSize=3 is 
+%                                          recommended. 
+%                                        - For 'very strong' noise strength (e.g., noise variance larger 
+%                                          than the data's dynamic range) optionalParams.maxBinSize=5 is 
+%                                          recommended.
+%                                      NOTE: optionalParams.maxBinSize must be odd and smaller or equal 
+%                                      than 5; if not, the algorithm automatically converts it to the 
+%                                      smallest odd value closest to the input and larger or equal than 1.
+%                                      
+%                                      If optionalParams.maxBinSize is not specified, the algorithm will 
+%                                      estimate it internally.
+%
+%  'optionalParams.enableDeblurring' : enable deblurring.
+%
+% 'optionalParams.deblurringStrenght' : adjusts the deblurring PSF width. Only used if optionalParams.PSF 
+%                                       is not specified. It must be a strictly positive scalar that is 
+%                                       used to adjust the width of the default PSF used for deblurring.
+%
+%                 'optionalParams.PSF': 2D PSF used for deblurring. If given as input, it overwrites the 
+%                                       default isotropic PSF and it ignores optionalParams.deblurringStrenght.
+%
+%  OUTPUT:
+%
+%      'noisy' : noisy data read from inputPath (M x N double array)
+%
+%   'denoised' : denoised data (M x N double array)
+%
+%  'deblurred' : deblurred data (if deblurring is executed, otherwise empty) (M x N double array)
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+% Copyright (c) 2006-2022 Tampere University.
+% All rights reserved.
+% This work (software, material, and documentation) shall only
+% be used for nonprofit noncommercial purposes.
+% Any unauthorized use of this work for commercial or for-profit purposes
+% is prohibited.
+%
+% AUTHOR:
+%     L. Azzari
+%     email: lucio.azzari@tuni.fi
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 %% check depndencies and add subfolder paths for auxiliary functions
 fprintf('*--------------------------------------------------------------*\n');
