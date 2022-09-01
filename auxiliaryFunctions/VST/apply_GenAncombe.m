2c524bc30bd3916176d82dd4eb245f25f6533469
Lucio Azzari <lucio.azzari@gmail.com>
2022-09-01 11:08:29 +0300

    fix bug

--- auxiliaryFunctions/VST/apply_GenAncombe.m
+++ auxiliaryFunctions/VST/apply_GenAncombe.m
@@ -4,9 +4,22 @@ alpha = p(1);
 sigma = sqrt(max(p(2), 0));
 g = max(-p(2), 0) / alpha;
 
-if forward
-    out = GenAnscombe_forward(in, sigma, alpha, g);
+if alpha > 0
+    if forward
+        out = GenAnscombe_forward(in, sigma, alpha, g);
+    else
+        %     out = GenAnscombe_inverse_closed_form(in,sigma,alpha,g);
+        out = GenAnscombe_inverse_exact_unbiased(in,sigma,alpha,g);
+    end
 else
-%     out = GenAnscombe_inverse_closed_form(in,sigma,alpha,g);
-    out = GenAnscombe_inverse_exact_unbiased(in,sigma,alpha,g);
+    disp('Warning: Estimated parameter a negative! Folding back to signal-independent noise model');
+    if sigma>0
+        if forward
+            out = in/sigma;
+        else
+            out = in*sigma;
+        end
+    else
+        out = in;
+    end
 end
