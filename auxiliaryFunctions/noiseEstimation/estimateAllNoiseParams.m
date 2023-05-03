88189364195005dba69b252ee8476de6923d16be
Lucio Azzari <lucio.azzari@tuni.fi>
2023-05-03 10:17:08 +0300

    print noise info

--- auxiliaryFunctions/noiseEstimation/estimateAllNoiseParams.m
+++ auxiliaryFunctions/noiseEstimation/estimateAllNoiseParams.m
@@ -5,3 +5,10 @@ if strcmp(noiseModel,'colored')
     ratio = getNoiseScalingFactor(data,noiseParams);
     noiseParams = noiseParams*ratio;
 end
+
+if numel(noiseParams)==2
+    disp(['Estimated signal-independent noise parameters: a = ' num2str(noiseParams(1)) ', b = ' num2str(noiseParams(2))...
+        '. For the mean value of the sequence, that is , ' num2str(mean(data(:))) ', the noise has a standard deviation of ' num2str(sqrt(noiseParams(1)*mean(data(:))+noiseParams(2))) '.'])
+elseif numel(noiseParams)==1
+    disp(['Estimated signal-independent noise parameters: b = ' num2str(noiseParams(1)) '.']);
+end
